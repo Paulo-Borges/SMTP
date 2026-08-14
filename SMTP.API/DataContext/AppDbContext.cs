48a2570de1c56abdb85ae8da41735d5faf330cc9
Borges <borgespaulo72@yahoo.com.br>
2026-08-14 18:47:04 -0300

    feat: Iniciando MembroModel

--- SMTP.API/DataContext/AppDbContext.cs
+++ SMTP.API/DataContext/AppDbContext.cs
@@ -11,5 +11,6 @@ namespace SMTP.API.DataContext
         }
 
         public DbSet<UserModel> Users { get; set; }
+        public DbSet<MembroModel> Membros { get; set; }
     }
 }
---
+++ SMTP.API/Models/MembroModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SMTP.API.Models
+{
+    public class MembroModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Cargo { get; set; }
+
+    }
+}
