69a16c3dedfe873f2686879800f2a8f28788ccc3
Borges <borgespaulo72@yahoo.com.br>
2026-08-18 15:56:10 -0300

    feat: Criando Migration de realacionamento entre User e Membro

---
+++ SMTP.API/Migrations/20260818184914_adicionandoRelacionamentoUserMembro.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMTP.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicionandoRelacionamentoUserMembro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Membros",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.Sql(@"
+        DECLARE @UserPadraoId INT;
+
+        SELECT TOP 1 @UserPadraoId = Id
+        FROM Users
+        ORDER BY Id;
+
+        IF @UserPadraoId IS NULL
+        BEGIN
+            INSERT INTO Users (Nome, Email, Cpf)
+            VALUES ('Usuário Legado', 'legado@local', '00000000000');
+
+            SET @UserPadraoId = SCOPE_IDENTITY();
+        END
+
+        UPDATE Membros
+        SET UserId = @UserPadraoId
+        WHERE UserId IS NULL;
+    ");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Membros",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Membros_UserId",
+                table: "Membros",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Membros_Users_UserId",
+                table: "Membros",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
