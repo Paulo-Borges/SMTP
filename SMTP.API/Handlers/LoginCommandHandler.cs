80aabb91a9e738667ab55421847cb6eaccd39c0f
Borges <borgespaulo72@yahoo.com.br>
2026-08-19 15:47:08 -0300

    fix: Configurando JWT

--- SMTP.API/Handlers/LoginCommandHandler.cs
+++ SMTP.API/Handlers/LoginCommandHandler.cs
@@ -1,25 +1,56 @@
 ﻿using MediatR;
-using SMTP.API.Controllers; // Garante o acesso ao LoginCommand
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SMTP.API.Controllers;
+using SMTP.API.DataContext;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text; // Garante o acesso ao LoginCommand
 
 namespace SMTP.API.Handlers
 {
     // 1. Alterado de "AppController.LoginCommand" para apenas "LoginCommand"
     public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
     {
-        // 2. Removido o ponto e vírgula ';' após a assinatura do método e adicionadas as chaves { }
+        private readonly IConfiguration _config;
+        private readonly AppDbContext _context;
+
+        public LoginCommandHandler(IConfiguration config, AppDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
         public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
+            var user = await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == request.Email, cancellationToken);
 
-            return await Task.FromResult("TokenJWTDeExemploAqui12345");
+            if (user is null)
+            {
+                throw new UnauthorizedAccessException("Usuário ou senha inválidos.");
+            }
 
+            var claims = new[]
+            {
+            new Claim("nome", user.Nome),
+            new Claim("email", user.Email),
+            new Claim("cpf", user.Cpf),
+        };
 
-            //// 1. Lógica de autenticação--------X------MOCKADO--------------X-------------------
-            //if (request.Email == "admin@email.com" && request.Password == "123456")
-            //{
-            //    return "TokenJWTDeExemploAqui12345";
-            //}
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            //throw new UnauthorizedAccessException();
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: creds
+            );
+
+            return await Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
         }
+
     }
+    
 }
